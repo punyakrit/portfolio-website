6823d28db3ab0bceeb67c2919016a907d5e2b4c7
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2024-12-25 05:02:24 +0530

    navbar motion

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,48 +2,64 @@
 import { Briefcase, Folder, Home, NotebookIcon } from "lucide-react";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { motion } from "motion/react";
 
 function NavBar() {
   const router = useRouter();
 
   return (
     <div className="flex justify-center">
-      <div className=" px-7 py-3 bg-[#1b1b19] fixed  top-6 rounded-2xl ">
+      <motion.div
+        initial={{ scale: 0.7 }}
+        animate={{
+          scale: 1,
+          transition: { duration: 1 },
+        }}
+        className=" px-7 py-3 bg-[#1b1b19] fixed  top-6 rounded-2xl "
+      >
         <div className="space-x-12 flex">
-          <div
+          <motion.div
+            whileHover={{ scale: 1.3}}
+            whileTap={{ scale: 0.9 }}
             className="cursor-pointer"
             onClick={() => {
               router.push("/");
             }}
           >
             <Home />
-          </div>
-          <div
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.9 }}
             className="cursor-pointer"
             onClick={() => {
               router.push("/about");
             }}
           >
             <Folder />
-          </div>
-          <div
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.9 }}
             className="cursor-pointer"
             onClick={() => {
               router.push("/work-experience");
             }}
           >
             <Briefcase />
-          </div>
-          <div
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.9 }}
             className="cursor-pointer"
             onClick={() => {
               router.push("/contact");
             }}
           >
             <NotebookIcon />
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
