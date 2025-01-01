eea5239330c4f5691dbfebe43b7b5d422d25e18e
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-01-01 23:59:33 +0530

    header build

--- components/ui/Home.tsx
+++ components/ui/Home.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ProfleCard from "./ProfleCard";
+import ProfleCard from "../ProfleCard";
+import SectionHandler from "./home/SectionHandler";
 
 function Home() {
   return (
-    <div className="flex">
-      <div className="w-1/3">
+    <div className="flex h-screen">
+      <div className="w-2/5 ">
         <ProfleCard />
       </div>
-      <div>
-        hi
+      <div className="ml-14 pt-44 w-3/5 overflow-y-scroll ">
+        <SectionHandler/>
       </div>
     </div>
   );
---
+++ components/ui/home/SectionHandler.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function SectionHandler() {
+  return (
+    <div className="mr-10">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default SectionHandler;
