f415fa84a2cda072cf8b84e1d1084f8f7759fa21
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-01-05 01:02:42 +0530

    card

--- components/ProfleCard.tsx
+++ components/ProfleCard.tsx
@@ -7,44 +7,48 @@ import { motion } from "motion/react";
 import Link from "next/link";
 function ProfleCard() {
   return (
-    <div className=" h-screen flex justify-end items-center">
+    <div className="h-screen flex justify-end items-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 1, ease: "easeInOut" } }}
-        className="w-2/3 h-2/3 bg-white rounded-3xl p-8 flex flex-col justify-between items-center"
+        className="w-2/3 h-2/3 bg-white rounded-3xl p-8 flex flex-col justify-between items-center relative"
       >
+       
+
         <div className="">
-          <Image alt="image" src={image} className="rounded-3xl " />
+          <div className="px-5">
+            <Image alt="image" src={image} className="rounded-3xl" />
+          </div>
           <div className="text-black text-center mt-4 font-bold text-3xl">
             Punyakrit Singh Makhni
           </div>
         </div>
+       
+
         <div>
           <div className="text-black/70 text-xl text-center">
             A Software Engineer who has developed countless innovative
             solutions.
           </div>
-          <div>
-            <div className="text-orange-500 flex justify-center space-x-8 mt-6 ">
-              <Link href="https://github.com/punyakrit" target="_blank">
-                <Github />
-              </Link>
-              <Link
-                href="https://www.linkedin.com/in/punyakrit-singh-makhni/"
-                target="_blank"
-              >
-                <Linkedin />
-              </Link>
-              <Link
-                href="mailto:punyakritsinghmakhni@gmail.com"
-                target="_blank"
-              >
-                <Mail />
-              </Link>
-              <Link href="https://x.com/Punyakrit22" target="_blank">
-                <X />
-              </Link>
-            </div>
+          <div className="text-orange-500 flex justify-center space-x-8 mt-6">
+            <Link href="https://github.com/punyakrit" target="_blank">
+              <Github />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/punyakrit-singh-makhni/"
+              target="_blank"
+            >
+              <Linkedin />
+            </Link>
+            <Link
+              href="mailto:punyakritsinghmakhni@gmail.com"
+              target="_blank"
+            >
+              <Mail />
+            </Link>
+            <Link href="https://x.com/Punyakrit22" target="_blank">
+              <X />
+            </Link>
           </div>
         </div>
       </motion.div>
