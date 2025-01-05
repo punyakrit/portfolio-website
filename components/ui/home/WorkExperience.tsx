b20cbd3657a92f1eb6091a048c91c494cad17496
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-01-06 01:14:37 +0530

    shadcn

--- components/ui/home/WorkExperience.tsx
+++ components/ui/home/WorkExperience.tsx
@@ -29,8 +29,9 @@ function WorkExperience() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1.8, ease: "easeInOut" } }}
+      className="pb-4"
     >
-      <div className="text-8xl font-extrabold text-wrap leading-none w-3/4">
+      <div className="text-8xl font-extrabold text-wrap leading-none w-3/4 ">
         <span className="">Work </span>
         <span className="text-[#343334]">Experience</span>
       </div>
