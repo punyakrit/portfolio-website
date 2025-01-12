f06875028eba9a0ff85cb5a83dc2d2994427058e
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-01-12 19:38:49 +0530

    ui

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex justify-center my-20'>
+      <span className=''>Made By{" "} </span><span className='text-orange-500'>Punyakrit</span> 
+    </div>
+  )
+}
+
+export default Footer
--- components/ui/Home.tsx
+++ components/ui/Home.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ProfleCard from "../ProfleCard";
 import SectionHandler from "./home/SectionHandler";
+import Footer from "../Footer";
 
 function Home() {
   return (
-    <div className="flex h-screen">
-      <div className="w-2/5 ">
-        <ProfleCard />
-      </div>
-      <div className="ml-14 pt-44 w-3/5 overflow-y-scroll ">
-        <SectionHandler/>
+    <div>
+      <div className="flex h-screen">
+        <div className="w-2/5 sticky">
+          <ProfleCard />
+        </div>
+        <div className="ml-14 pt-44 w-3/5 overflow-y-scroll ">
+          <SectionHandler />
+        </div>
       </div>
     </div>
   );
